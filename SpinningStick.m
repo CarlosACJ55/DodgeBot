0329c1bf5045be09253f12a86d0748c08a879fa8
Samotoda <111883623+Samotoda@users.noreply.github.com>
2024-01-12 19:42:26 -0500

    Updated A1

--- SpinningStick.m
+++ SpinningStick.m
@@ -4,18 +4,18 @@ clc
 syms theta(t) phi(t)
 
 gravity = 9.80665;    % m/s^2
-rod_mass = 1/2;       % kg
-rod_radius = 0.375;   % Meters
+disk_mass = 1;       % kg
+disk_radius = 3 * pi / 16;   % Meters
 T_e = 40;             % Motor Torque Nm
-J_rod = 1/12 * rod_mass * rod_radius ^ 2;
-T_L = rod_mass * gravity * cos(theta);
+J_rod = 1/2 * disk_mass * disk_radius ^ 2;
+T_L = disk_mass * gravity * cos(theta);
 
 %For starting parameters:
 cond1 = pi / 4; %Initial Rotor position
 cond2 = 0;      %Initial Rotor speed
 %cond3 = alpha(0) == 0;      %Initial Rotor acceleration
 
-[V] = odeToVectorField(diff(phi, 2) == 1/J_rod * (T_e - rod_mass * gravity * cos(phi)));
+[V] = odeToVectorField(diff(phi, 2) == 1/J_rod * (T_e));
 M = matlabFunction(V,'vars', {'t','Y'});
 tspan = linspace(0, 0.1, 1000);
 y0 = [cond1 cond2];
