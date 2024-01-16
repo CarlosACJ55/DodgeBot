b01d490620fda6d5b36bc95b33460b114ef8fe31
Samotoda <111883623+Samotoda@users.noreply.github.com>
2024-01-16 14:42:00 -0500

    Added STM & Motor Spin Files

--- SpinningStick.m
+++ SpinningStick.m
@@ -6,9 +6,9 @@ syms theta(t) phi(t)
 gravity = 9.80665;    % m/s^2
 rod_mass = 1/2;       % kg
 rod_radius = 0.375;   % Meters
-T_e = 40;             % Motor Torque Nm
+T_e = 30;             % Motor Torque Nm
 J_rod = 1/3 * rod_mass * rod_radius ^ 2;
-T_L = rod_mass * gravity * cos(phi);
+T_L = rod_mass * gravity * rod_radius * cos(phi);
 
 %For starting parameters:
 cond1 = pi / 4; %Initial Rotor position
