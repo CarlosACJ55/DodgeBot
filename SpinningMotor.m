06ee5d43f63f1f33d4b9c2951c039c26dca9622e
Samotoda <inkurudoaura@gmail.com>
2024-01-17 15:47:22 -0500

    Updated SpinningMotor Calculation

--- SpinningMotor.m
+++ SpinningMotor.m
@@ -4,18 +4,17 @@ clc
 syms theta(t) phi(t)
 
 gravity = 9.80665;    % m/s^2
-rod_mass = 20;       % kg
-rod_radius = ;   % Meters
-T_e = 30;             % Motor Torque Nm
-J_rod = 1/2 * rod_mass * rod_radius ^ 2;
-T_L = rod_mass * gravity * rod_radius * cos(phi);
+disk_mass = 23;       % kg
+disk_radius = 0.102 / 2 * sqrt(2);   % Meters
+T_e = 60;             % Motor Torque Nm
+J_motors = 1/2 * disk_mass * disk_radius ^ 2;
 
 %For starting parameters:
 cond1 = pi / 4; %Initial Rotor position
 cond2 = 0;      %Initial Rotor speed
 %cond3 = alpha(0) == 0;      %Initial Rotor acceleration
 
-[V] = odeToVectorField(diff(phi, 2) == 1/J_rod * (T_e));
+[V] = odeToVectorField(diff(phi, 2) == 1/J_motors * (T_e));
 M = matlabFunction(V,'vars', {'t','Y'});
 tspan = linspace(0, 0.1, 1000);
 y0 = [cond1 cond2];
