1718756238c6bb97a991be2b4ce0b3f5aab42344
CarlosACJ55 <CarlosACJ55@icloud.com>
2024-01-30 17:38:26 -0500

    merged python files

--- Camera_Testing/camera_calibration.py
+++ Camera_Testing/camera_calibration.py
@@ -1,52 +1,41 @@
 import numpy as np
 
-# from sklearn.cluster import KMeans
-
 # print(cv.cuda.getDevice())
 centroid_buffer1 = [np.zeros(2, dtype=float)]
 print(centroid_buffer1)
 print("\n")
-
-image_sensor_width = 5.44  # mm (horizonal)
+image_sensor_width = 5.44  # mm (horizontal)
 image_sensor_height = 3.072  # mm (vertical)
 ratio = 16 / 9
 focal_length = 2.9  # mm
 
 # https://en.wikipedia.org/wiki/Angle_of_view_(photography)#Calculating_a_camera%27s_angle_of_view
-horizonal_fov = 2 * np.arctan(image_sensor_width / (2 * focal_length))
+horizontal_fov = 2 * np.arctan(image_sensor_width / (2 * focal_length))
 vertical_fov = 2 * np.arctan(image_sensor_height / (2 * focal_length))
 diagonal_fov = 2 * np.arctan(np.sqrt(image_sensor_height ** 2 + image_sensor_width ** 2) / (2 * focal_length))
-print(horizonal_fov)
+print(horizontal_fov)
 print(vertical_fov)
 print(diagonal_fov)
 
 
 # def pixel_2_cords(row, col):
 
-def pixel_2_cords(pixel_location, pixel_center_location, center_dist, camera_height, user_height, camera_orentation):
-    real_ratio = user_height / camera_height
-    real_center_dist = real_ratio * center_dist
-
+def pixel_2_cords(pixel_location, pixel_center_location, center_dist, camera_height, height, orientation):
+    real_ratio = height / camera_height
+    dist = real_ratio * center_dist
     pixel_xy = pixel_center_location - pixel_location
     pixel_xy_ratio = pixel_xy / pixel_center_location
-    real_dist_cords = pixel_xy_ratio * real_center_dist
-    return camera_orentation * real_dist_cords
+    real_dist_cords = pixel_xy_ratio * dist
+    return orientation * real_dist_cords
 
 
-def cords_2_pixel(real_dist_cords,
-                  pixel_center_location,
-                  center_dist,
-                  camera_height,
-                  user_height,
-                  inv_camera_orientation):
-    real_ratio = user_height / camera_height
-    real_center_dist = real_ratio * center_dist
-
+def cords_2_pixel(real_dist_cords, pixel_center_location, center_dist, camera_height, height, inv_camera_orientation):
+    real_ratio = height / camera_height
+    dist = real_ratio * center_dist
     orientated_cords = real_dist_cords * inv_camera_orientation
-    pixel_xy_ratio = orientated_cords / real_center_dist
+    pixel_xy_ratio = orientated_cords / dist
     pixel_xy = pixel_xy_ratio * pixel_center_location
     pixel_location = pixel_center_location - pixel_xy
-
     return pixel_location.astype('uint16')
 
 
@@ -56,10 +45,8 @@ real_center_dist = 1.06
 cam_height = 2
 user_height = 1.01
 camera_orientation = np.array([-1, 1])
-
 xy = pixel_2_cords(pixel_loc, center, real_center_dist, cam_height, user_height, camera_orientation)
 xy_pix = cords_2_pixel(xy, center, real_center_dist, cam_height, user_height, camera_orientation)
 print(xy)
 print(xy_pix)
-
 print(np.random.choice([-1, 0, 1]))
--- Camera_Testing/camera_test2.py
+++ Camera_Testing/camera_test2.py
@@ -45,30 +45,22 @@ def calibration_circle(frame):
     return frame
 
 
-def pixel_2_cords(pixel_location, pixel_center_location, center_dist, camera_height, user_height, camera_orentation):
-    real_ratio = user_height / camera_height
-    real_center_dist = real_ratio * center_dist
-
+def pixel_2_cords(pixel_location, pixel_center_location, center_dist, camera_height, height, orientation):
+    real_ratio = height / camera_height
+    dist = real_ratio * center_dist
     pixel_xy = pixel_center_location - pixel_location
     pixel_xy_ratio = pixel_xy / pixel_center_location
-    real_dist_cords = pixel_xy_ratio * real_center_dist
-    return camera_orentation * real_dist_cords
+    real_dist_cords = pixel_xy_ratio * dist
+    return orientation * real_dist_cords
 
 
-def cords_2_pixel(real_dist_cords,
-                  pixel_center_location,
-                  center_dist,
-                  camera_height,
-                  user_height,
-                  inv_camera_orientation):
-    real_ratio = user_height / camera_height
-    real_center_dist = real_ratio * center_dist
-
+def cords_2_pixel(real_dist_cords, pixel_center_location, center_dist, camera_height, height, inv_camera_orientation):
+    real_ratio = height / camera_height
+    dist = real_ratio * center_dist
     orientated_cords = real_dist_cords * inv_camera_orientation
-    pixel_xy_ratio = orientated_cords / real_center_dist
+    pixel_xy_ratio = orientated_cords / dist
     pixel_xy = pixel_xy_ratio * pixel_center_location
     pixel_location = pixel_center_location - pixel_xy
-
     return pixel_location.astype('uint16')
 
 
@@ -76,7 +68,6 @@ def kmeans_gpu(identified_punch_cords, num_gloves, prev_cluster_centers):
     if identified_punch_cords.size > 0:
         identified_punch_cords_torch = torch.from_numpy(identified_punch_cords)
         try:
-<<<<<<< Updated upstream
             if prev_cluster_centers[0, 0] == -10:
 
                 # st = time.time()
@@ -85,17 +76,6 @@ def kmeans_gpu(identified_punch_cords, num_gloves, prev_cluster_centers):
                                                        device='cuda:0')
             else:
                 cluster_labels = kmeans_predict(identified_punch_cords_torch, prev_cluster_centers, device='cuda:0')
-
-=======
-            # if prev_cluster_centers[0,0] == -10:
-
-            # st = time.time()
-            # if prev_cluster_centers[0][0] == -10:
-            cluster_labels, punch_centers = kmeans(X=identified_punch_cords_torch, num_clusters=num_gloves, device='cuda:0')
-            # else:
-                # cluster_labels = kmeans_predict(identified_punch_cords_torch, prev_cluster_centers, device='cuda:0')
-                
->>>>>>> Stashed changes
             # KMeans(n_clusters=num_gloves, n_init=10, algorithm="lloyd")
 
             # kmeans.fit(identified_punch_cords)
@@ -113,15 +93,9 @@ def kmeans_gpu(identified_punch_cords, num_gloves, prev_cluster_centers):
 
             return punch_pixels, punch_centers
         except ValueError as e:
-<<<<<<< Updated upstream
             return [identified_punch_cords], torch.tensor([[-10, -10], [-10, -10]])
     else:
         return [], torch.tensor([[-10, -10], [-10, -10]])
-=======
-            return [identified_punch_cords],torch.tensor([[0, 0],[0,0]])
-    else:
-        return [], torch.tensor([[0, 0],[0,0]])
->>>>>>> Stashed changes
 
 
 st = time.time()
@@ -138,16 +112,13 @@ cam_height = 2
 # Max - 1.35
 # Ayman - 1.524
 user_height = 1.49
-
-camera_orentation = np.array([-1, 1])
-
+camera_orientation = np.array([-1, 1])
 buffer_size = 3
 centroid_buffer1 = [np.zeros(2, dtype=float)] * buffer_size
 centroid_buffer2 = [np.zeros(2, dtype=float)] * buffer_size
-
 previous_robot_loc = np.array([0, 0])
 previous_robot_loc_pixel = cords_2_pixel(previous_robot_loc, pixel_center, real_center_dist, cam_height, user_height,
-                                         camera_orentation)
+                                         camera_orientation)
 max_dodge_dist = .1
 avoidance_dist = .15
 cord_centers = torch.tensor([[-10, -10], [-10, -10]])
@@ -161,10 +132,8 @@ while cap.isOpened():
         print("Can't receive frame (stream end?). Exiting ...")
         break
     # Convert the frame to the HSV color space (better for color tracking)
-
     hsv_frame = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
     print("color con:", time.time() - st)
-
     lower_bound_gloves = np.array([0, 100, 100])
     upper_bound_gloves = np.array([15, 255, 255])
     lower_bound_bot = np.array([130, 100, 100])
@@ -172,36 +141,29 @@ while cap.isOpened():
     # for yellow target
     lower_bound_gloves = np.array([20, 100, 100])
     upper_bound_gloves = np.array([30, 255, 255])
-
     # lower_bound_bot = np.array([130, 100, 100])
     # upper_bound_bot = np.array([160, 255,255])
     # Create a binary mask for the specified color
     color_mask_gloves = cv.inRange(hsv_frame, lower_bound_gloves, upper_bound_gloves)
     print("range:", time.time() - st)
     # color_mask_bot = cv.inRange(hsv_frame, lower_bound_bot, upper_bound_bot)
-
     # color_mask_bot = cv.inRange(hsv_frame, lower_bound_bot, upper_bound_bot)
     # color_mask = color_mask / 255
     result = cv.bitwise_and(frame, frame, mask=color_mask_gloves)
     # print("bitwise &", time.time() - st)
     # results = cv.bitwise_and(frame)
-
     # rgb_frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
-
     # frame_shape = rgb_frame.shape
     # vectorized = rgb_frame.reshape((-1,3))
-
     # results = cv.bitwise_and(frame)
-
     # color_mask_3d = np.stack((color_mask,) * 3, axis=-1)
-
     # Find the coordinates of all marked color pixels in the mask
     # finds all the pixels where the condition is true
     # the returns a list of cordinates where its true
     marked_pixel_coords = np.column_stack(np.where(color_mask_gloves > 0))
 
     marked_coords = pixel_2_cords(marked_pixel_coords, pixel_center, real_center_dist, cam_height, user_height,
-                                  camera_orentation)
+                                  camera_orientation)
     # cv.imshow('frame', marked_pixel_coords)
     # binary_frame = color_mask_3d * frame
     # binary_frame = np.logical_and(hsv_frame, color_mask_3d)
@@ -211,7 +173,6 @@ while cap.isOpened():
     cord_list, cord_centers = kmeans_gpu(marked_coords, 2, cord_centers)
     print("cord centers", cord_centers.shape)
     print("kmeans:", time.time() - st)
-
     if len(cord_list) == 2:
         # print("centroid:", time.time()-st)
         # centroid1 = np.mean(cord_list[0], axis=0, dtype=int)
@@ -224,47 +185,35 @@ while cap.isOpened():
         real_centroid2 = centroid2  # pixel_2_cords(centroid2, pixel_center, real_center_dist, cam_height,
         # user_height, camera_orentation)
         print("pixel2cords:", time.time() - st)
-
-        # inserts the new cord
-
         if len(centroid_buffer1) >= buffer_size and len(centroid_buffer1) >= buffer_size:
-            # takes out the last vaules in the buffer
             centroid_buffer1.pop()
             centroid_buffer2.pop()
         centroid_buffer1.insert(0, real_centroid1)
         centroid_buffer2.insert(0, real_centroid2)
-
         print("buffer:", time.time() - st)
-
         punch1_dist = np.linalg.norm(centroid_buffer1[0] - previous_robot_loc)
         punch2_dist = np.linalg.norm(centroid_buffer2[0] - previous_robot_loc)
-
         if punch1_dist > punch2_dist:
             main_punch = centroid_buffer1
             main_punch_dist = punch1_dist
             prev_punch_dist = np.linalg.norm(centroid_buffer1[buffer_size - 1] - previous_robot_loc)
             centroid = cords_2_pixel(centroid1, pixel_center, real_center_dist, cam_height, user_height,
-                                     camera_orentation)
+                                     camera_orientation)
             centroid_ = cords_2_pixel(centroid2, pixel_center, real_center_dist, cam_height, user_height,
-                                      camera_orentation)
-
+                                      camera_orientation)
         else:
             main_punch = centroid_buffer2
             main_punch_dist = punch2_dist
             prev_punch_dist = np.linalg.norm(centroid_buffer2[buffer_size - 1] - previous_robot_loc)
             centroid = cords_2_pixel(centroid2, pixel_center, real_center_dist, cam_height, user_height,
-                                     camera_orentation)
+                                     camera_orientation)
             centroid_ = cords_2_pixel(centroid1, pixel_center, real_center_dist, cam_height, user_height,
-                                      camera_orentation)
+                                      camera_orientation)
 
         print("norm:", time.time() - st)
-
         # mag_buffer1 = np.linalg.norm(np.abs(centroid_buffer1[len(centroid_buffer1)-1] - centroid_buffer1[0]))
         # mag_buffer2 = np.linalg.norm(np.abs(centroid_buffer2[len(centroid_buffer2)-1] - centroid_buffer2[0]))
-
-
         # checks to see if rate of change is greater than .2 ie, saying that punch has begun
-
         # if mag_buffer1 > .2 or mag_buffer2 > .2:
         #     if mag_buffer1 > mag_buffer2:
         #         main_punch = centroid_buffer1
@@ -274,105 +223,57 @@ while cap.isOpened():
         #     main_punch = centroid_buffer2
         #     centroid = centroid2
         #     centroid_ = centroid1
-
-
         # punch_trajectory_line_slope = (main_punch[len(main_punch)-1][1] - main_punch[0][1]) / (main_punch[len(
         # main_punch)-1][0] - main_punch[0][0])
-
         punch_trajectory_line_slope = (main_punch[1][1] - main_punch[0][1]) / (main_punch[1][0] - main_punch[0][0])
         punch_trajectory_line_offset = main_punch[0][1] - punch_trajectory_line_slope * main_punch[0][0]
         punch_y_check = punch_trajectory_line_slope * previous_robot_loc[0] + punch_trajectory_line_offset
-
         dist_from_punch_traj = np.linalg.norm(np.array([previous_robot_loc[0], punch_y_check]) - previous_robot_loc)
-
         print("slope:", time.time() - st)
-
         vector_2frames = main_punch[0] - main_punch[1]
         norm_vector_2frames = np.linalg.norm(vector_2frames)
         hat_vector_2frames = vector_2frames / norm_vector_2frames
-
         if main_punch_dist < prev_punch_dist:
             if dist_from_punch_traj <= avoidance_dist:
-
                 if previous_robot_loc[0] >= 0:
                     vx = -1 * np.abs(hat_vector_2frames[0])
                 else:
                     vx = np.abs(hat_vector_2frames[0])
-<<<<<<< Updated upstream
-
                 if previous_robot_loc[0] >= 0:
-=======
-                
-                if previous_robot_loc[1] >= 0:
->>>>>>> Stashed changes
                     vy = -1 * np.abs(hat_vector_2frames[1])
                 else:
                     vy = np.abs(hat_vector_2frames[1])
-
                 hat_avoidance_vector = np.array([vx, vy])
-<<<<<<< Updated upstream
-
                 if norm_vector_2frames > .05 and main_punch_dist < .75:
                     print("its coming")
                     previous_robot_loc = previous_robot_loc + (
-                                avoidance_dist - dist_from_punch_traj) * hat_avoidance_vector + (
-                                                     avoidance_dist / 2) * hat_avoidance_vector
-
+                            avoidance_dist - dist_from_punch_traj) * hat_avoidance_vector + (
+                                                 avoidance_dist / 2) * hat_avoidance_vector
         previous_robot_loc_pixel = cords_2_pixel(previous_robot_loc, pixel_center, real_center_dist, cam_height,
-                                                 user_height, camera_orentation)
-
-=======
-                
-                
-                # if norm_vector_2frames > .05 and main_punch_dist < .75:
-                print("its coming")
-                previous_robot_loc = previous_robot_loc + (avoidance_dist - dist_from_punch_traj)*hat_avoidance_vector + (avoidance_dist/2)*hat_avoidance_vector
-        
-        previous_robot_loc_pixel = cords_2_pixel(previous_robot_loc, pixel_center, real_center_dist, cam_height, user_height, camera_orentation)
-        
-            
->>>>>>> Stashed changes
+                                                 user_height, camera_orientation)
         cv.circle(frame, tuple(centroid[::-1]), 5, (0, 0, 255), -1)
         cv.circle(frame, tuple(centroid_[::-1]), 5, (0, 255, 0), -1)
         cv.circle(frame, tuple(previous_robot_loc_pixel[::-1]), 15, (255, 255, 255), -1)
-
-
     else:
         print("away")
         # print(previous_robot_loc)
-<<<<<<< Updated upstream
         cord_centers = torch.tensor([[-10, -10], [-10, -10]])
         if (abs(previous_robot_loc[0]) >= .5) or (abs(previous_robot_loc[1]) >= .5) or np.isnan(
                 previous_robot_loc).any():
             previous_robot_loc = np.array([0, 0])
             previous_robot_loc_pixel = cords_2_pixel(previous_robot_loc, pixel_center, real_center_dist, cam_height,
-                                                     user_height, camera_orentation)
-=======
-        cord_centers = torch.tensor([[0, 0],[0,0]])
-        if (abs(previous_robot_loc[0]) >= .5) or (abs(previous_robot_loc[1]) >= .5) or np.isnan(previous_robot_loc).any():
-            previous_robot_loc = np.array([0,0])
-            previous_robot_loc_pixel = cords_2_pixel(previous_robot_loc,pixel_center, real_center_dist, cam_height, user_height, camera_orentation)
->>>>>>> Stashed changes
+                                                     user_height, camera_orientation)
         cv.circle(frame, tuple(previous_robot_loc_pixel[::-1]), 15, (0, 0, 0), -1)
-
     # Calculate the centroid (average position) of red pixels
     # if marked_pixel_coords.size > 0:
     # centroid = np.mean(marked_pixel_coords, axis=0, dtype=int)
     # print(pixel_2_cords(pixel_center, centroid, real_center_dist, cam_height, user_height, camera_orentation))
-
     # calibration circle
     # frame = calibration_circle(frame)
-
-
     # bgr_binary_frame = cv.cvtColor(binary_frame, cv.COLOR_HSV2BGR)
     print("end:", time.time() - st)
     cv.imshow('Original Frame', frame)
-
     if cv.waitKey(1) == ord('q'):
         break
-
-    # i += 1
-
-# Release the capture and close OpenCV windows
 cap.release()
 cv.destroyAllWindows()
