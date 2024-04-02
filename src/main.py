b494e430f8c6b2ebc0738259f96c7c6452e703b0
CarlosACJ55 <CarlosACJ55@icloud.com>
2024-04-02 15:40:44 -0400

    Cleared repo to reorganize with poetry and git commands.

--- src/main.py
+++
@@ -1,9 +0,0 @@
-from communication.protocol import Protocol
-from game.game import Game
-from gui.root import Root
-
-if __name__ == "__main__":
-    stm = Protocol()
-    ui = Root(stm)
-    game = Game(stm, ui)
-    ui.show()
