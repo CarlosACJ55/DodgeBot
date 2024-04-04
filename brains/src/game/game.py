be9d450af5e6103bcb3300072d2353af998c027a
CarlosACJ55 <CarlosACJ55@icloud.com>
2024-04-04 10:39:13 -0400

    Improved game

--- brains/src/game/game.py
+++ brains/src/game/game.py
@@ -1,6 +1,6 @@
 import threading
+import time
 
-from src.communication import codes
 from src.game.state import Phase, State
 from src.pathfiner.pathfinder import Pathfinder
 
@@ -16,12 +16,18 @@ class Game:
     def configure(self, height, time):
         self.state.height = height
         self.state.time = time
-        if self.stm.transition(codes.SYNC):
+        if self.stm.transition(Phase.IDLE):
             self.state.phase = Phase.IDLE
 
+    def reset(self):
+        pos = self.stm.find()
+        self.stm.move(-pos.x, -pos.y)
+
     def end(self):
-        self.stm.reset()
+        self.reset()
         self.state.phase = Phase.IDLE
+        if not self.stm.transition(Phase.IDLE):
+            self.emergency_reset()
         self.frame = self.ui.menu_frame(self)
 
     def emergency_reset(self):
@@ -30,32 +36,39 @@ class Game:
         self.state.phase = Phase.RESET
         if self.state.phase == Phase.IN_GAME:
             self.end()
-        if self.stm.reset():
+        self.reset()
+        pos = self.stm.find()
+        if pos.x or pos.y:
+            self.stm.power_off()
+        if self.stm.transition(Phase.IDLE):
             self.state.phase = Phase.IDLE
         else:
-            self.stm.power_off()
-        self.stm.disconnect()
+            self.stm.disconnect()
         self.state.phase = Phase.DISCONNECT
 
     def countdown(self):
         for self.state.time in reversed(range(self.state.time)):
             self.frame.update_timer()
-        self.end()
 
     def start_dodging(self):
         pf = Pathfinder(self.state.height)
+        last_punch = time.time()
         while self.state.phase == Phase.IN_GAME:
-            dodge_path = pf.detect_punch()
-            if dodge_path is not None:
-                self.stm.move_to(dodge_path)
-            self.stm.reset()
+            dodge = pf.detect_punch()
+            if dodge:
+                self.stm.move(*dodge)
+                last_punch = time.time()
+            if time.time() - last_punch > 2:
+                self.stm.reset()
 
     def play(self):
         if self.stm.check_connection() and self.state.phase == Phase.IDLE:
-            self.frame = self.ui.timer_frame(self.state)
+            self.frame = self.ui.timer_frame(self)
+            self.state.phase = Phase.IN_GAME
             time_thread = threading.Thread(target=self.countdown)
             time_thread.start()
-            self.state.phase = Phase.IN_GAME
+            if not self.stm.transition(Phase.IN_GAME):
+                self.emergency_reset()
             self.start_dodging()
             time_thread.join()
             self.end()
